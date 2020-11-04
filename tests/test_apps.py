ec412477c301d9bb918d8a3eee90c390f1678d17
Sam Watson <samydan@hotmail.co.uk>
2020-11-04 17:42:22 +0000

    readme

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,13 +1,7 @@
 import requests
 
-def capital_case(x):
-    return x.capitalize()
-
-def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
-
 def test_nginx():
-    "GET request to url returns a 502"
+    "GET request to url returns a 200"
     url = 'http://192.168.10.100/'
     resp = requests.get(url)
     assert resp.status_code == 200
@@ -18,30 +12,14 @@ def test_nginx_html():
     resp = requests.get(url)
     assert resp.headers['content-type'] == 'text/html'
 
-def test_home():
-    "GET request to url returns a 502"
-    url = 'http://192.168.10.100/'
-    resp = requests.get(url)
-    assert resp.status_code == 200
-
-def test_app():
+def test_app1():
     "GET request to url returns a 200"
     url = 'http://192.168.10.101/'
     resp = requests.get(url)
     assert resp.status_code == 200
 
-def test_app1():
+def test_app2():
     "GET request to url returns a 200"
     url = 'http://192.168.10.102/'
     resp = requests.get(url)
     assert resp.status_code == 200
-
-# url = 'https://w3schools.com/python/demopage.php'
-# x = requests.get(url, proxies = { "https" : "https://1.1.0.1:80"})
-# print(x.text)
-
-def test_app1():
-    "GET request to url returns a 200"
-    url = 'http://192.168.10.100'
-    resp = requests.get(url, proxies = { "http" : "http://192.168.10.100"})
-    assert resp.text == 200
